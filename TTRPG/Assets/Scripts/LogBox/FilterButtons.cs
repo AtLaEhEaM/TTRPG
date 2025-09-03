903746da4803da22aef369eff964499716e39176
AtLaEhEaM <99670121+AtLaEhEaM@users.noreply.github.com>
2025-09-03 22:22:14 +0500

    filter buttons

--- TTRPG/Assets/Scripts/LogBox/FilterButtons.cs
+++ TTRPG/Assets/Scripts/LogBox/FilterButtons.cs
@@ -1,16 +1,58 @@
+using TMPro;
 using UnityEngine;
 
 public class FilterButtons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject logBoxParent;
+    private TextMeshProUGUI[] logBoxes;
+
+    private LogBoxType? activeFilter = null; // null = no filter
+
+    private void GetBoxes()
     {
-        
+        logBoxes = logBoxParent.GetComponentsInChildren<TextMeshProUGUI>(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FilterFarm()
     {
-        
+        ToggleFilter(LogBoxType.Farm);
+    }
+
+    public void FilterArmy()
+    {
+        ToggleFilter(LogBoxType.Army);
+    }
+
+    public void FilterScout()
+    {
+        ToggleFilter(LogBoxType.Scout);
+    }
+
+    private void ToggleFilter(LogBoxType type)
+    {
+        GetBoxes();
+
+        // if the same filter is active, reset
+        if (activeFilter == type)
+        {
+            ResetBoxes();
+            activeFilter = null;
+            return;
+        }
+
+        // apply new filter
+        foreach (var box in logBoxes)
+        {
+            var info = box.GetComponent<LogBoxInfo>();
+            box.gameObject.SetActive(info != null && info._type == type);
+        }
+
+        activeFilter = type;
+    }
+
+    private void ResetBoxes()
+    {
+        foreach (var box in logBoxes)
+            box.gameObject.SetActive(true);
     }
 }
--- TTRPG/Assets/Scripts/LogBox/LogBoxInfo.cs
+++ TTRPG/Assets/Scripts/LogBox/LogBoxInfo.cs
@@ -5,9 +5,12 @@ public class LogBoxInfo : MonoBehaviour
     public LogBoxType _type;
     [HideInInspector]
     public Transform _transform;
+    [HideInInspector]
+    public GameObject _gameObject;
 
     public void Start()
     {
+        _gameObject = GetComponent<GameObject>();
         _transform = GetComponent<Transform>();
     }
 
